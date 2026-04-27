644ecad350c4754b6f9e351feeabd5dfee68c846
Tak1naa <2901005357@qq.com>
2026-04-27 17:20:49 +0800

    修正未使用的import语句

--- backend/src/main/java/com/canteen/client/impl/CppSeatClientImpl.java
+++ backend/src/main/java/com/canteen/client/impl/CppSeatClientImpl.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
